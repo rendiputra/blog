8cea1548d0113b396b74e8ee559b10eebc2a68fb
rendiputra <rendiputrapradana@gmail.com>
2026-02-19 00:17:37 +0700

    setup deployment CI/CD github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rendiputra.github.io',
+  base: '/blog',
+
   output: 'static',
   integrations: [
     tailwind(),
